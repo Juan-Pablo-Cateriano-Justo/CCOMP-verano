0a77791dffbace844f3f17d0434315ab77697c6c
Juan Pablo Cateriano Justo <juanpablocateriano@gmail.com>
2026-01-23 17:54:39 -0500

    Implement 2D shape hierarchy with polymorphism

--- Clase 23/01.cpp
+++ Clase 23/01.cpp
@@ -126,9 +126,9 @@ private:
 //Se pide diseñar una jerarquia de clases usando herencia que cumpla: UNa clase base llamada Estructura. Una clase derivada Vector. Una clase derivada Matriz. 
 //TOdos deben Almacenar sus datos dinámicamente, conocer sus dimensiones y tener método mostrarDimensiones(); 
 // ¿que atributos deben ir en clase base? ¿cuales deben ser protedted para que las 
-#include <iostream>
-#include <string>
-using namespace std; 
+//#include <iostream>
+//#include <string>
+//using namespace std; 
 
 /*
 class Estructura {
@@ -194,6 +194,7 @@ int main(){
 
 }
 */
+/*
 #include <iostream>
 #include <string>
 #include <cmath>
@@ -276,3 +277,119 @@ public:
         return M_PI * radio * radio;
     }
 };
+*/
+
+
+
+
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+class Figura2D {
+protected:
+	int x;
+	int y;
+	string color;
+	
+public:
+	Figura2D(int x, int y, string c) : x(x), y(y), color(c) {}
+	
+	virtual void mostrarInfo() {
+		cout << "Punto x: " << x << endl;
+		cout << "Punto y: " << y << endl;
+		cout << "Color: " << color << endl;
+	}
+	
+	virtual double Area() const = 0;
+	
+	virtual ~Figura2D() {}
+};
+
+class Linea : public Figura2D {
+protected:
+	int x2;
+	int y2;
+	
+public:
+	Linea(int x, int y, string c, int x2, int y2)
+		: Figura2D(x, y, c), x2(x2), y2(y2) {}
+	
+	void mostrarInfo() override {
+		Figura2D::mostrarInfo();
+		cout << "Segundo punto x: " << x2 << endl;
+		cout << "Segundo punto y: " << y2 << endl;
+	}
+	
+	double Area() const override {
+		return 0.0; 
+	}
+};
+
+class Rectangulo : public Figura2D {
+private:
+	int base;
+	int altura;
+	
+public:
+	Rectangulo(int x, int y, string c, int b, int a)
+		: Figura2D(x, y, c), base(b), altura(a) {}
+	
+	void mostrarInfo() override {
+		Figura2D::mostrarInfo();
+		cout << "Base: " << base << endl;
+		cout << "Altura: " << altura << endl;
+	}
+	
+	double Area() const override {
+		return base * altura;
+	}
+};
+
+class Circulo : public Figura2D {
+private:
+	int radio;
+	
+public:
+	Circulo(int x, int y, string c, int r)
+		: Figura2D(x, y, c), radio(r) {}
+	
+	void mostrarInfo() override {
+		Figura2D::mostrarInfo();
+		cout << "Radio: " << radio << endl;
+	}
+	
+	double Area() const override {
+		return M_PI * radio * radio;
+	}
+};
+
+
+int main() {
+
+    // Punteros a la clase base (polimorfismo)
+    Figura2D* f1 = new Linea(0, 0, "Rojo", 4, 5);
+    Figura2D* f2 = new Rectangulo(2, 3, "Azul", 6, 4);
+    Figura2D* f3 = new Circulo(5, 5, "Verde", 3);
+
+    cout << "=== Linea ===" << endl;
+    f1->mostrarInfo();
+    cout << "Area: " << f1->Area() << endl << endl;
+
+    cout << "=== Rectangulo ===" << endl;
+    f2->mostrarInfo();
+    cout << "Area: " << f2->Area() << endl << endl;
+
+    cout << "=== Circulo ===" << endl;
+    f3->mostrarInfo();
+    cout << "Area: " << f3->Area() << endl << endl;
+
+    // Liberar memoria
+    delete f1;
+    delete f2;
+    delete f3;
+
+    return 0;
+}
+
