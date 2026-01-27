629b317faba0681c31bf464a886dcc49d6ddbe78
Juan Pablo Cateriano Justo <juanpablocateriano@gmail.com>
2026-01-27 17:23:50 -0500

    Create 02.cpp

---
+++ Clase 23/02.cpp
@@ -0,0 +1,47 @@
+#include <iostream> 
+#include <vector>
+using namespace std; 
+
+
+int main(){
+  vector<int> v; 
+
+
+//insertar elementos 
+v.push_back(10);
+v.push_back(20);
+v.push_back(30); 
+
+  cout<< "Vector inicial: " 
+  for(int i=0 ; v.size() ; i++)
+    cout<< v[i] << " "; 
+  
+
+  //Elminar ultimo elemento 
+  v.pop_back(); 
+
+  cout<< "Luego de pop_back(): ";
+  for*int i=0 ; v.size() ; i++)
+    cout<< v[i] << " "; 
+  cout<< endl; 
+
+  //verificar si esta vacio
+
+  if(v.empty())
+    cout<< " El vector esta vacio " << endl;
+
+  else
+    cout<< "El vector No esta vacio" << endl; 
+
+  //limpiar todo
+
+  v.clear();
+  cout<< "Luego de clear(), tamano = " << v.size() << endl; 
+
+  return 0;
+
+
+
+}
+  
+} 
